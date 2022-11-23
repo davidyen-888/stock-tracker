165f1285c3ee2acd147847db4b65224e19601f69
David Hsieh <yen888yen888@gmail.com>
2022-11-23 09:36:41 -0800

    Search: fetch data success

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,5 +1,55 @@
+import { useEffect, useState } from "react";
+import finnhub from "../api/finnhub";
+
 const Search = () => {
-  return <h1>Search</h1>;
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    let isMounted = true;
+    const fetchData = async () => {
+      try {
+        const response = await finnhub.get("/search", {
+          params: {
+            q: search,
+          },
+        });
+        // console.log(response);
+        if (isMounted) {
+          setResults(response.data.result);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    // only call fetchData if search is not empty
+    if (search.length > 0) {
+      fetchData();
+    } else {
+      setResults([]);
+    }
+    return () => {
+      isMounted = false;
+    };
+  }, [search]);
+
+  return (
+    <div className="w-50 p-5 rounded mx-auto">
+      <div className="form-floating dropdown">
+        <input
+          style={{ backgroundColor: "rgba(145, 158, 171, 0.04" }}
+          id="search"
+          type="text"
+          className="form-control"
+          placeholder="Search"
+          autoComplete="off"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        ></input>
+        <label htmlFor="search">Search</label>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
